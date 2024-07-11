e8e59b0d96b934bc81bc0603aa069b2923d9a2c1
Lucas Gonzalez <luc4s92.g@gmail.com>
2024-07-11 19:29:53 -0300

    Benefits section created

--- src/constants/imgs.ts
+++ src/constants/imgs.ts
@@ -26,3 +26,56 @@ export const companyLogos = [
   { src: "/yourlogo.svg", alt: "Company Logo" },
   { src: "/yourlogo.svg", alt: "Company Logo" },
 ];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Ask anything",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "/benefits/card-1.svg",
+    iconUrl: "/benefits/icon-1.svg",
+    imageUrl: "/benefits/image-2.png",
+  },
+  {
+    id: "1",
+    title: "Improve everyday",
+    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    backgroundUrl: "/benefits/card-2.svg",
+    iconUrl: "/benefits/icon-2.svg",
+    imageUrl: "/benefits/image-2.png",
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Connect everywhere",
+    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
+    backgroundUrl: "/benefits/card-3.svg",
+    iconUrl: "/benefits/icon-3.svg",
+    imageUrl: "/benefits/image-2.png",
+  },
+  {
+    id: "3",
+    title: "Fast responding",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "/benefits/card-4.svg",
+    iconUrl: "/benefits/icon-4.svg",
+    imageUrl: "/benefits/image-2.png",
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Ask anything",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "/benefits/card-5.svg",
+    iconUrl: "/benefits/icon-1.svg",
+    imageUrl: "/benefits/image-2.png",
+  },
+  {
+    id: "5",
+    title: "Improve everyday",
+    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    backgroundUrl: "/benefits/card-6.svg",
+    iconUrl: "/benefits/icon-2.svg",
+    imageUrl: "/benefits/image-2.png",
+  },
+];
