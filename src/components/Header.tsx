a7a6fcd5e2112d4ae487b05a00452944002fb61e
Lucas Gonzalez <luc4s92.g@gmail.com>
2024-09-05 15:37:55 -0300

    styles fixed on header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a href="#hero" className="block w-[12rem] px-5 xl:px-10 max-lg:py-4">
+        <a href="#hero" className="block w-[12rem] xl:mr-8">
           <img src="/brainwave.svg" width={190} height={40} alt="Brainwave" />
         </a>
         <nav
